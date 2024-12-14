60055b96824a55a28019236b7903af6dc5a63041
Chinu <165071337+ohlolsurmom@users.noreply.github.com>
2024-12-14 22:17:23 +0530

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,10 @@
+import openai
+
+prompt = """
+You are a helpful assistant that provides accurate information about Indian laws. 
+Provide concise answers to user questions based on the following guidelines:
+1. Do not offer personal legal advice.
+2. Stick to general legal principles under Indian law.
+
+User Question: {}
+"""
